00e54a4756f5936e9eb03940773f1b403370acf4
Brandon Corey <bcorey85@gmail.com>
2020-02-14 08:39:15 -0600

    add geocode and custom error handling

--- server.js
+++ server.js
@@ -1,12 +1,48 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+const colors = require('colors');
+const errorHandler = require('./middleware/error');
 
+//Load env vars
 dotenv.config({ path: './config/config.env' });
+
+//Connect to db
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+// Body parser
+app.use(express.json());
+
+//Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
+
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-	console.log(`Server started in ${process.env.NODE_ENV} on port ${PORT}`);
+const server = app.listen(PORT, () => {
+	console.log(
+		`Server started in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+	);
+});
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+	console.log(`Error: ${err.message}`.red);
+
+	//Close server & exit process
+	server.close(() => {
+		process.exit(1);
+	});
 });
