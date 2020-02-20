104cb877ca9c0a1d69e8bcfca2e63e865b85087e
Brandon Corey <bcorey85@gmail.com>
2020-02-20 17:05:18 -0600

    add reset password, update password, update details functionality

--- seeder.js
+++ seeder.js
@@ -37,7 +37,7 @@ const importData = async () => {
 	try {
 		await Bootcamp.create(bootcamps);
 		await Course.create(courses);
-		await Users.create(users);
+		await User.create(users);
 
 		console.log('Data imported'.green.inverse);
 		process.exit();
